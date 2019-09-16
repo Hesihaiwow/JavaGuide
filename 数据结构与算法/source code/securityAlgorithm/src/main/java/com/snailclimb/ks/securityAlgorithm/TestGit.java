45579eb220cf2414a93c76b372e2e15f370e18c0
hesihai <1780606227@qq.com>
2019-09-16 16:40:43 +0800

    TestGit

---
+++ 数据结构与算法/source code/securityAlgorithm/src/main/java/com/snailclimb/ks/securityAlgorithm/TestGit.java
@@ -0,0 +1,16 @@
+package com.snailclimb.ks.securityAlgorithm;
+
+/**
+ * @program: JavaGuide
+ * @description:
+ * @author: Mr.He
+ * @create: 2019-09-16 16:38
+ **/
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+
+
+        System.out.println("hello world");
+    }
+}
